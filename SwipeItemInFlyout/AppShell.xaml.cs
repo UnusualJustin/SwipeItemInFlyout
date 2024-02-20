7330481577bdb2f27da10a1eb3e43abe380a235a
Justin Cook <justincook2003.work@gmail.com>
2024-02-20 09:48:52 -0600

    Add project files.

---
+++ SwipeItemInFlyout/AppShell.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace SwipeItemInFlyout
+{
+    public class SimpleCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            Application.Current.MainPage.DisplayAlert("This alert shows on Android, but not iOS.", "OK", "Cancel");
+        }
+    }
+
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        public SimpleCommand DoSomethingCommand => new SimpleCommand();
+    }
+}
---
+++ SwipeItemInFlyout/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SwipeItemInFlyout
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
